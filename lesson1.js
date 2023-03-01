177aadcdfbc234a38f7d6d471c0ca5aa554e6282
wilku_acer <albert.adam.wos@gmail.com>
2023-03-01 18:29:58 +0900

    first commit

---
+++ lesson1.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const PLNPrice = 0.033023;
+
+let item;
+let itemPrice;
+let priceOfItem;
+
+let result = false;
+do
+{
+    item = prompt("What is the item's name?", "coca-cola");
+    itemPrice = prompt("What is the item's price?", 120);
+    if(item == null || itemPrice == 0)
+    {
+        break;
+    }
+    priceOfItem = PLNPrice * itemPrice;
+
+    let feedbackInfo = `Price of ${item} is ${priceOfItem}`; 
+    alert(feedbackInfo);
+    result = confirm("Do you want to check another item?", [false]);
+}
+while(result);
+
+let fetchPromise = fetch("something.jpg").then(response =>
+    {
+        console.log("Fetch complete" + response);
+        return response.blob();
+    }).then(response =>
+    {
+        document.getElementById("picture").src = URL.createObjectURL(response);
+    })
