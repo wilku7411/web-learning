e691660ecc95f3a030d8e1b135584213775daf0f
wilku_acer <albert.adam.wos@gmail.com>
2023-03-15 16:34:58 +0900

    Added function, that show the current ISS data json

--- lesson_json.js
+++ lesson_json.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const ISSAPIURL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+async function whereISS()
+{
+    const response = await fetch(ISSAPIURL);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.latitude);
+    console.log(data.longitude);
+}
+whereISS();
